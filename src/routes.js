43942881b002b34e18c4c22349d9ffb491454af0
Lucas Wundervald <wundervald_2@hotmail.com>
2019-07-25 00:19:36 -0300

    finishing the model

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'asd' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'lucas',
+    email: 'lucas2',
+    password_hash: '123',
+  });
+  return res.json({ message: user });
+});
 
 export default routes;
