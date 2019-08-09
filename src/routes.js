6025716e6a50e41648c8b2dbf4bcbae69bc56b37
lucas <wundervald.lucas@gmail.com>
2019-08-09 15:02:58 -0300

    add password hash

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'lucas',
-    email: 'lucas2',
-    password_hash: '1234',
-  });
-  return res.json({ message: user });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
